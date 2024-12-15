2b4c1322573c6185dc1a88145baca6692f424592
Mostafiz1257 <mostafiz1257@gmail.com>
2024-12-15 12:42:50 +0600

    implement login functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,19 @@
 "use client";
+import { useLoginMutation } from "@/src/redux/features/auth/authApi";
+import { setUser } from "@/src/redux/features/auth/authSlice";
+import { useAppDispatch } from "@/src/redux/hooks";
+
+import { TUser } from "@/src/types/type.auth";
+import { verifyToken } from "@/src/utils/verityToken";
 import { Input } from "@nextui-org/input";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { toast } from "sonner"; 
 
 const LoginPage = () => {
+  const [login, { isLoading }] = useLoginMutation();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
   const [formData, setFormData] = useState({ email: "", password: "" });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -10,14 +21,24 @@ const LoginPage = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
+
     e.preventDefault();
-    console.log("Form Data:", formData);
+    try {
+      const res = await login(formData).unwrap();
+      console.log(res);
+      const user = verifyToken(res.token) as TUser;
+      dispatch(setUser({ user, token: res.token }));
+      toast.success("Login successful!");
+      router.push("/");
+    } catch (error: any) {
+      toast.error(error?.data?.message || "Login failed. Please try again.");
+    }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center ">
-      <div className=" shadow-lg rounded-lg p-6 w-full max-w-md">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="shadow-lg rounded-lg p-6 w-full max-w-md">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -47,9 +68,12 @@ const LoginPage = () => {
           <div className="flex justify-center">
             <button
               type="submit"
-              className="bg-blue-600  py-2 px-4 rounded-xl hover:bg-blue-700 transition duration-200"
+              className={`bg-blue-600 py-2 px-4 rounded-xl hover:bg-blue-700 transition duration-200 ${
+                isLoading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              disabled={isLoading}
             >
-              Login Now
+              {isLoading ? "Logging in..." : "Login Now"}
             </button>
           </div>
         </form>
--- src/components/navabr/Navbar.tsx
+++ src/components/navabr/Navbar.tsx
@@ -1,64 +1,84 @@
-import Link from 'next/link';
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+"use client";
+import { currentUser } from "@/src/redux/features/auth/authSlice";
+import { useAppSelector } from "@/src/redux/hooks";
+import Link from "next/link";
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
+  const user = useAppSelector(currentUser);
+  console.log(user?.role);
   return (
-    <nav className="bg-black shadow-md sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+    <nav className='bg-black shadow-md sticky top-0 z-50'>
+      <div className='container mx-auto px-4 py-4 flex justify-between items-center'>
         {/* Logo */}
-        <Link href={'/'}>
-        <div className="text-2xl font-bold text-white">E3DS</div>
+        <Link href={"/"}>
+          <div className='text-2xl font-bold text-white'>E3DS</div>
         </Link>
-     
 
         {/* Navigation Links */}
-        <ul className="hidden md:flex space-x-8 text-white font-medium">
-        <Link href={'/'}>
-          <li className="hover:text-[#003580] cursor-pointer">Home</li>
+        <ul className='hidden md:flex space-x-8 text-white font-medium'>
+          <Link href={"/"}>
+            <li className='hover:text-[#003580] cursor-pointer'>Home</li>
           </Link>
-          <Link href={'/admin'}>
-          <li className="hover:text-[#003580] cursor-pointer">About/Admin</li>
-          </Link>
-          <li className="hover:text-[#003580] cursor-pointer">Products</li>
-          <li className="hover:text-[#003580] cursor-pointer">Pricing</li>
-          <li className="hover:text-[#003580] cursor-pointer">Solutions</li>
-          <li className="hover:text-[#003580] cursor-pointer">Resources</li>
-          <li className="hover:text-[#003580] cursor-pointer">Support</li>
-      
+
+          <li className='hover:text-[#003580] cursor-pointer'>Products</li>
+          <li className='hover:text-[#003580] cursor-pointer'>Pricing</li>
+          <li className='hover:text-[#003580] cursor-pointer'>Solutions</li>
+          <li className='hover:text-[#003580] cursor-pointer'>Resources</li>
+          <li className='hover:text-[#003580] cursor-pointer'>Support</li>
+          {user && user.role == "admin" ? (
+            <Link href={"/admin"}>
+              <li className='hover:text-blue-800 border border-1 px-3 rounded-full  border-blue-700 text-blue-700 cursor-pointer'>Admin</li>
+            </Link>
+          ) : (
+            <></>
+          )}
         </ul>
 
         {/* Social Icons */}
-        <div className="flex space-x-4 text-white items-center justify-center">
-          <FaFacebook className="hover:text-[#003580] cursor-pointer" size={20} />
-          <FaTwitter className="hover:text-[#003580] cursor-pointer" size={20} />
-          <FaInstagram className="hover:text-[#003580] cursor-pointer" size={20} />
-          <FaLinkedin className="hover:text-[#003580] cursor-pointer" size={20} />
-          <Link href={'/login'}>
-          <button className="px-4 py-2 bg-blue-600 rounded-3xl hover:bg-blue-700 transition duration-300">
-         Login Now
-        </button>
-        </Link>
+        <div className='flex space-x-4 text-white items-center justify-center'>
+          <FaFacebook
+            className='hover:text-[#003580] cursor-pointer'
+            size={20}
+          />
+          <FaTwitter
+            className='hover:text-[#003580] cursor-pointer'
+            size={20}
+          />
+          <FaInstagram
+            className='hover:text-[#003580] cursor-pointer'
+            size={20}
+          />
+          <FaLinkedin
+            className='hover:text-[#003580] cursor-pointer'
+            size={20}
+          />
+          <Link href={"/login"}>
+            <button className='px-4 py-2 bg-blue-600 rounded-3xl hover:bg-blue-700 transition duration-300'>
+              Login Now
+            </button>
+          </Link>
         </div>
 
         {/* Mobile Menu Button */}
-        <div className="md:hidden flex items-center">
+        <div className='md:hidden flex items-center'>
           <button
-            className="text-gray-700 focus:outline-none"
-            aria-label="Open menu"
+            className='text-gray-700 focus:outline-none'
+            aria-label='Open menu'
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
+                d='M4 6h16M4 12h16m-7 6h7'
               />
             </svg>
           </button>
